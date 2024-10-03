2eb3fb9ac7c5c17705ac20c2d1a8a15300d914c9
pratikk2609 <pratikpawar2609@gmail.com>
2024-10-03 20:31:40 +0530

    demo commit

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -28,9 +28,9 @@
     methods: {
       handleLogin() {
         // Mock login logic for demonstration
-        if (this.username === 's1' && this.password === 's1') {
+        if (this.username === 's' && this.password === 's') {
           this.$router.push('/student');
-        } else if (this.username === 'hr1' && this.password === 'hr1') {
+        } else if (this.username === 'hr' && this.password === 'hr') {
           this.$router.push('/hr');
         } else {
           this.errorMessage = 'Invalid credentials. Please try again.';
